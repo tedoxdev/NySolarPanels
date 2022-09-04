8824fcd843155a9379518533d9513182a4bb1476
tedoxdev <67103406+tedoxdev@users.noreply.github.com>
2022-09-04 13:13:29 +0200

    Update for ECO 9.6.x

--- NySolarPanels.cs
+++ NySolarPanels.cs
@@ -1,9 +1,10 @@
+
 namespace Eco.Mods.TechTree
 {
-    // [DoNotLocalize]
     using System;
     using System.Collections.Generic;
     using System.ComponentModel;
+    using Eco.Core.Items;
     using Eco.Gameplay.Blocks;
     using Eco.Gameplay.Components;
     using Eco.Gameplay.Components.Auth;
@@ -12,6 +13,7 @@ namespace Eco.Mods.TechTree
     using Eco.Gameplay.Housing;
     using Eco.Gameplay.Interactions;
     using Eco.Gameplay.Items;
+    using Eco.Gameplay.Modules;
     using Eco.Gameplay.Minimap;
     using Eco.Gameplay.Objects;
     using Eco.Gameplay.Players;
@@ -30,89 +32,94 @@ namespace Eco.Mods.TechTree
     using Eco.Shared.Items;
     using Eco.Gameplay.Pipes;
     using Eco.World.Blocks;
-	
-	[Serialized]
-	[RequireComponent(typeof(OnOffComponent))]  
-	[RequireComponent(typeof(PropertyAuthComponent))]
-	[RequireComponent(typeof(PowerGridComponent))]              
+    using Eco.Gameplay.Housing.PropertyValues;
+    using static Eco.Gameplay.Housing.PropertyValues.HomeFurnishingValue;
+
+    [Serialized]
+    [RequireComponent(typeof(OnOffComponent))]
+    [RequireComponent(typeof(PropertyAuthComponent))]
+    [RequireComponent(typeof(PowerGridComponent))]
     [RequireComponent(typeof(PowerGeneratorComponent))]
-	[RequireComponent(typeof(HousingComponent))]                  
-    [RequireComponent(typeof(SolidGroundComponent))] 
-    public partial class NySolarPanelsObject : 
-        WorldObject,    
-        IRepresentsItem
+    [RequireComponent(typeof(HousingComponent))]
+    [RequireComponent(typeof(SolidAttachedSurfaceRequirementComponent))]
+    [PowerGenerator(typeof(ElectricPower))]
+    public partial class NySolarPanelsObject : WorldObject, IRepresentsItem
     {
-        public override LocString DisplayName { get { return Localizer.DoStr("NySolarPanels"); } } 
+        public override LocString DisplayName { get { return Localizer.DoStr("Solar Panels"); } }
+        public override TableTextureMode TableTexture => TableTextureMode.Metal;
+        public virtual Type RepresentedItemType { get { return typeof(NySolarPanelsItem); } }
 
-        public virtual Type RepresentedItemType { get { return typeof(NySolarPanelsItem); } } 
-
-        static NySolarPanelsObject()
+        protected override void Initialize()
         {
-            WorldObject.AddOccupancy<NySolarPanelsObject>(new List<BlockOccupancy>(){
-				new BlockOccupancy(new Vector3i(0, 0, 0)),
-				new BlockOccupancy(new Vector3i(0, 0, 1)),
-				new BlockOccupancy(new Vector3i(0, 0, -1)),
-            });
-        }
-		
-		protected override void Initialize()
-        {             
-            this.GetComponent<PowerGridComponent>().Initialize(30, new ElectricPower());        
-            this.GetComponent<PowerGeneratorComponent>().Initialize(500);                       
-            this.GetComponent<HousingComponent>().Set(SolarGeneratorItem.HousingVal);                                
+            this.ModsPreInitialize();
+            this.GetComponent<PowerGridComponent>().Initialize(30, new ElectricPower());
+            this.GetComponent<PowerGeneratorComponent>().Initialize(500);
+            this.GetComponent<HousingComponent>().HomeValue = NySolarPanelsItem.homeValue;
+            this.ModsPostInitialize();
         }
 
         public override void Destroy()
         {
             base.Destroy();
         }
-       
+
+        /// <summary>Hook for mods to customize WorldObject before initialization. You can change housing values here.</summary>
+        partial void ModsPreInitialize();
+        /// <summary>Hook for mods to customize WorldObject after initialization.</summary>
+        partial void ModsPostInitialize();
     }
 
     [Serialized]
-    public partial class NySolarPanelsItem :
-        WorldObjectItem<NySolarPanelsObject> 
+    [LocDisplayName("Solar Panels")]
+    [Ecopedia("Crafted Objects", "Power Generation", createAsSubPage: true, display: InPageTooltip.DynamicTooltip)]
+    public partial class NySolarPanelsItem : WorldObjectItem<NySolarPanelsObject>
     {
-        public override LocString DisplayName { get { return Localizer.DoStr("NySolarPanels"); } } 
-        public override LocString DisplayDescription  { get { return Localizer.DoStr("Solar panels"); } }
+        public override LocString DisplayDescription => Localizer.DoStr("Modern Solar Panels");
 
-        static NySolarPanelsItem()
+        public override DirectionAxisFlags RequiresSurfaceOnSides { get;} = 0 | DirectionAxisFlags.Down;
+
+        public override HomeFurnishingValue HomeValue => homeValue;
+        public static readonly HomeFurnishingValue homeValue = new HomeFurnishingValue()
         {
-            
-        }
-		
-		[TooltipChildren] public HousingValue HousingTooltip { get { return HousingVal; } }
-        [TooltipChildren] public static HousingValue HousingVal { get { return new HousingValue() 
-                                                {
-                                                    Category = "Industrial",
-                                                    TypeForRoomLimit = "", 
-        };}}
-        
-        [Tooltip(8)] private LocString PowerProductionTooltip  { get { return new LocString(string.Format(Localizer.DoStr("Produces: {0}w"), Text.Info(500))); } } 
-        
+            Category                 = RoomCategory.Industrial,
+            TypeForRoomLimit         = Localizer.DoStr(""),
+        };
+
+        [Tooltip(8)] private LocString PowerProductionTooltip  => Localizer.Do($"Produces: {Text.Info(500)}w of {new ElectricPower().Name} power");
     }
-	
-	[RequiresSkill(typeof(ElectronicsSkill), 0)]      
-    public partial class NySolarPanelsRecipe : Recipe
+
+    [RequiresSkill(typeof(ElectronicsSkill), 1)]
+    public partial class NySolarPanelsRecipe : RecipeFamily
     {
         public NySolarPanelsRecipe()
         {
-            this.Products = new CraftingElement[]
-            {
-                new CraftingElement<NySolarPanelsItem>(),
-            };
-
-            this.Ingredients = new CraftingElement[]
-            {
-                new CraftingElement<SteelItem>(typeof(ElectronicsSkill), 25, ElectronicsSkill.MultiplicativeStrategy, typeof(ElectronicsLavishResourcesTalent)),
-                new CraftingElement<CopperWiringItem>(typeof(ElectronicsSkill), 40, ElectronicsSkill.MultiplicativeStrategy, typeof(ElectronicsLavishResourcesTalent)),
-                new CraftingElement<CircuitItem>(typeof(ElectronicsSkill), 10, ElectronicsSkill.MultiplicativeStrategy, typeof(ElectronicsLavishResourcesTalent)),          
-            };
-            this.ExperienceOnCraft = 10;  
-            this.CraftMinutes = CreateCraftTimeValue(typeof(NySolarPanelsRecipe), Item.Get<NySolarPanelsItem>().UILink(), 40, typeof(ElectronicsSkill), typeof(ElectronicsFocusedSpeedTalent), typeof(ElectronicsParallelSpeedTalent));    
+            var recipe = new Recipe();
+            recipe.Init(
+                "SolarPanels",  //noloc
+                Localizer.DoStr("Solar Panels"),
+                new List<IngredientElement>
+                {
+                    new IngredientElement(typeof(SteelBarItem), 25, typeof(ElectronicsSkill), typeof(ElectronicsLavishResourcesTalent)),
+                    new IngredientElement(typeof(CopperWiringItem), 40, typeof(ElectronicsSkill), typeof(ElectronicsLavishResourcesTalent)),
+                    new IngredientElement(typeof(BasicCircuitItem), 10, typeof(ElectronicsSkill), typeof(ElectronicsLavishResourcesTalent)),
+                },
+                new List<CraftingElement>
+                {
+                    new CraftingElement<NySolarPanelsItem>()
+                });
+            this.Recipes = new List<Recipe> { recipe };
+            this.ExperienceOnCraft = 10;
+            this.LaborInCalories = CreateLaborInCaloriesValue(2000, typeof(ElectronicsSkill));
+            this.CraftMinutes = CreateCraftTimeValue(typeof(NySolarPanelsRecipe), 40, typeof(ElectronicsSkill), typeof(ElectronicsFocusedSpeedTalent), typeof(ElectronicsParallelSpeedTalent));
+            this.ModsPreInitialize();
             this.Initialize(Localizer.DoStr("Solar Panels"), typeof(NySolarPanelsRecipe));
+            this.ModsPostInitialize();
             CraftingComponent.AddRecipe(typeof(ElectronicsAssemblyObject), this);
         }
+
+        /// <summary>Hook for mods to customize RecipeFamily before initialization. You can change recipes, xp, labor, time here.</summary>
+        partial void ModsPreInitialize();
+        /// <summary>Hook for mods to customize RecipeFamily after initialization, but before registration. You can change skill requirements here.</summary>
+        partial void ModsPostInitialize();
     }
-	
 }
